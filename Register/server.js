3c2285dad9f2e5caa039b8293d736e1aae12370a
satwikj <satwikj21@gmail.com>
2020-07-20 21:10:53 +0530

    Project

---
+++ Register/server.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyparser = require('body-parser');
+var app = express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin,x-Requested-With,Content-Type,Accept");
+    next();
+});
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "sarita@123",
+    database: "mainproject"
+});
+connection.connect(function(err){
+    if(!err){
+        console.log("Database Connected.....");
+    }
+    else{
+        console.log("Error in connecting database.........");
+    }
+});
+
+app.listen(4002,()=>{
+    console.log("Server is Running");
+});
+
+app.get('/',function(req,res){
+    res.json({msg:"Welcome to Node.js API"});
+});
+
+//Route for Insert Data
+app.post('/register',(req,res) =>{
+    let data = req.body;
+    //To see the data in console
+    console.log(data);
+    connection.query('insert into register1 set ? ',data,function(error,results,fields){
+        if(error){
+            res.send({
+                "code":400,
+                "failed":"Error Occured"
+            });
+        }
+        else{
+            res.send({
+                "code":200,
+                "Success":"User Registeration Completed"
+            });
+        }
+    });
+});
+
+app.post('/login',(req,res) =>{
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(username,password);
+    connection.query('select username,password from register1 where username = ? and password = ?',[username,password],function(error,results,fields){
+        var x = JSON.stringify(results);
+        if(x == "[]"){
+            res.send({
+                "code":400,
+                "failed":"Cant find Username and Password"
+            });
+        }
+        else{
+            res.send({
+                "code":200,
+                "success":"Successful Login"
+            });
+        }
+    });
+});
--- valid.js
+++ valid.js
@@ -7,10 +7,10 @@ var address = document.forms['form']['address'];
 
 var name1_error = document.getElementById('name1_error');
 var mobile_error = document.getElementById('mobile_error');
- var email_error = document.getElementById('email_error');
- var user_error = document.getElementById('user_error');
- var password_error = document.getElementById('password_error');
- var address_error = document.getElementById('address_error');
+var email_error = document.getElementById('email_error');
+var user_error = document.getElementById('user_error');
+var password_error = document.getElementById('password_error');
+var address_error = document.getElementById('address_error');
 
 name1.addEventListener('textInput',name1_verify);
 mobile.addEventListener('textInput',mobile_verify);
@@ -19,121 +19,163 @@ user.addEventListener('textInput',user_verify);
 password.addEventListener('textInput',password_verify);
 address.addEventListener('textInput',address_verify);
 
- function validated(){
-    var a =/[a-z]{4,50}/g;
-    var m =/[6-9][0-9]{9}/g;
-    var e = /[a-zA-Z._0-9-]{6,12}[@][a-z]{3,15}[.][a-z]{2,6}/g;
-    var u =/[a-z]{4,8}/g;
-    var p = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$#%^&()*]).{5,12}/g;
-     
-    if(name1.value.match(u))
-    {  
-    }
-    else{
+function validated(){
+   var x =0 ;
+   var a =/[a-z]{4,50}/g;
+   var m =/[6-9][0-9]{9}/g;
+   var e = /[a-zA-Z._0-9-]{2,12}[@][a-z]{3,15}[.][a-z]{2,6}/g;
+   var u =/[a-z]{2,12}/g;
+   var p = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$#%^&()*]).{5,12}/g;
+   if(name1.value.match(u)){
+      x=x+1;  
+   }
+   else{
       name1.focus();
       name1_error.style.display = "block";
       return false;
+   }
 
-    }
-
-    if(mobile.value.match(m))
-    {
-       
-    }
-    else{
+    
+   var m =/[6-9][0-9]{9}/g;
+   if(mobile.value.match(m)){
+      x = x+1; 
+   }
+   else{
       mobile.focus();
       mobile_error.style.display = "block";
       return false;
+   }
 
-    }
-  
-    if(user.value.match(u))
-    {
-       
-    }
-    else{
-
+   var u =/[a-z]{2,12}/g;
+   if(user.value.match(u)){
+      x = x+1;
+   }
+   else{
       user.focus();
       user_error.style.display = "block";
       return false;
+   }
 
-    }
-    if(password.value.match(p))
-    {
-     
-    }
-    else{
+   var p = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$#%^&()*]).{5,12}/g;
+   if(password.value.match(p)){
+      x =x+1;
+   }
+   else{
       password.focus();
       password_error.style.display = "block";
       return false;
-    }
-    if(email.value.match(e))
-    {
-       
-    }
-    else{
+   }
+
+   var e = /[a-zA-Z._0-9-]{2,12}[@][a-z]{3,15}[.][a-z]{2,6}/g;
+   if(email.value.match(e)){ 
+      x=x+1;
+   }
+   else{
       email.focus();
       email_error.style.display = "block";
       return false;
+   }
 
-    }
-    if(address.value.match(a))
-    {
-       
-    }
-    else{
+   var a =/[a-z]{4,50}/g;
+   if(address.value.match(a)){
+      x = x+1;
+   }
+   else{
       address.focus();
       address_error.style.display = "block";
       return false;
-
-    }
+   }
+   
+   if(x == 6){
+      var data = {};
+      data.name = $('#id1').val(),
+      data.mobile = $('#id2').val(),
+      data.username = $('#id3').val(),
+      data.password = $('#id4').val(),
+      data.email = $('#id5').val(),
+      data.address = $('#id6').val(),
+         
+      $.ajax({
+         type : "POST" ,
+         url:"http://localhost:4002/register",
+         data : JSON.stringify(data),
+         contentType: "application/json; charset=utf-8",
+         dataType : "Json",
+         success:function(d) {
+            swal({
+               title: "Successfully Registered",
+               text: "Welcome to our Store",
+               icon: "success",
+               button: "Ok",
+           })
+           .then((value) => {
+               swal(window.location.href = 'login.html');
+            });    
+         },
+         error:function(tyjuk){
+            swal({
+               title: "Data not inserted in Database",
+               text: "Please Try Again",
+               icon: "error",
+               button: "Ok",
+           });
+         }
+      });
+   }
  }
- //verify functions
- function name1_verify(){
+ 
+
+name1.addEventListener('textInput',name1_verify);
+function name1_verify(){
    var u =/[a-z]{4,8}/g;
    if(name1.value.match(u)){
       name1_error.style.display = "none";
       return true;
-
    }
 }
+
+mobile.addEventListener('textInput',mobile_verify);
 function mobile_verify(){
    var m =/[6-9][0-9]{8}/g;
    if(mobile.value.match(m)){
       mobile_error.style.display = "none";
       return true;
-
    }
 }
+
+user.addEventListener('textInput',user_verify);
 function user_verify(){
    var u =/[a-z]{4,8}/g;
    if(user.value.match(u)){
       user_error.style.display = "none";
       return true;
-
    }
 }
+
+password.addEventListener('textInput',password_verify);
 function password_verify(){
    var p = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@$#%^&()*]).{5,12}/g;
    if(password.value.match(p)){
       password_error.style.display = "none";
       return true;
-
    }
 }
+
+email.addEventListener('textInput',email_verify);
 function email_verify(){
    var e = /[a-zA-Z._0-9-]{6,12}[@][a-z]{3,15}[.][a-z]{2,6}/g;
    if(email.value.match(e)){
       email_error.style.display = "none";
       return true;
-
    }
 }
+
+
+address.addEventListener('textInput',address_verify);
 function address_verify(){
    var a =/[a-z]{4,50}/g;
    if(address.value.match(a)){
       address_error.style.display = "none";
       return true;
-
    }
 }
